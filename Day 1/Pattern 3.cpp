949b27c7fe39c1e86679183973095fc27d20f0af
Tanishq Agarwal <76662143+tanishq6818@users.noreply.github.com>
2023-07-28 16:29:57 +0530

    Create Pattern 3.cpp

---
+++ Day 1/Pattern 3.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int sum=1;
+    for(int i=1;i<4;i++){
+        for(int j=1;j<4;j++){
+            cout<<sum++;
+        }
+        cout<<endl;
+    }
+}
